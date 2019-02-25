0bdaafa2a4e08f88dc2534d256ed329eaeea71a4
Ben Lebich <benlebich@gmail.com>
2019-02-24 19:56:12 -0800

    working scraper for single page

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var request = require('request');
 var scrape = {
-  url: 'http://www.benarea.com/',
+  url: 'https://advancedentalsmile.com/',
   imgType: ['png', 'jpg', 'jpeg'],
-  imgURLS: []
+  imgURL: [[],[],[]],
 };
 request(scrape.url, function (error, response, body) {
   scrapeImages(body);
@@ -14,20 +14,38 @@ function scrapeImages(rawData) {
       index: 0,
       totalImgs: 0,
       fullURL: false,
-      indexBack: 0
+      indexBack: 0,
+      url: "",
     }
-    console.log(`finding: ${scrape.imgType[x]}`);
+    console.log(`================= finding: ${scrape.imgType[x]} =================`);
     imgData.index = rawData.indexOf(`.${scrape.imgType[x]}`);
     imgData.indexBack = imgData.index;
-    if (imgData.index != -1) {
+    while (imgData.index != -1) {
+
       while(!imgData.fullURL) {
         imgData.indexBack--;
         // find index of the first double quote, single quote, space, and left parenthesis
         imgData.fullURL = (rawData[imgData.indexBack] != '"' && rawData[imgData.indexBack] != ' ' && rawData[imgData.indexBack] != '(') ? false : true;
-        console.log(rawData[imgData.indexBack]);
+      } // end while
+      imgData.fullURL = false;
+
+      //console.log(`Initial Index: ${imgData.index} Index Value: ${rawData[imgData.index]}`);
+      //console.log(`Last Index: ${imgData.indexBack} Last Value: ${rawData[imgData.indexBack]}`);
+
+      imgData.url = rawData.slice(imgData.indexBack + 1, imgData.index + scrape.imgType[x].length + 1);
+      rawData = rawData.replace(imgData.url, '');
+      if (imgData.url.length > (scrape.imgType[x].length + 1)) {
+        console.log(`Real URL: ${imgData.url}`);
+        imgData.totalImgs++;
+      } else {
+        console.log(`False Alarm: ${imgData.url}`);
       }
-      imgData.totalImgs++;
-    }
-    console.log(`Initial Index: ${imgData.index} Index Value: ${rawData[imgData.index]}\n\n`);
-  }
-}
+
+      imgData.index = rawData.indexOf(`.${scrape.imgType[x]}`);
+      imgData.indexBack = imgData.index;
+
+    } // end while
+
+    console.log();
+  } // end for
+} // end scrapeImages()
