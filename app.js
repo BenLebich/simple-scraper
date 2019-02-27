2f11c0b0f6370f9aa1142c4ca5c516add452ea70
Ben Lebich <benlebich@gmail.com>
2019-02-26 18:28:32 -0800

    added site link scraper function

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
 var request = require('request');
 var scrape = {
-  url: 'https://advancedentalsmile.com/',
+  url: 'http://www.benarea.com/',
   imgType: ['png', 'jpg', 'jpeg'],
   imgURL: [[],[],[]],
+  siteLinks: []
 };
 request(scrape.url, function (error, response, body) {
-  scrapeImages(body);
+  scrapeSiteLinks(body);
+  scrapeSiteImages(body);
 });
 
-function scrapeImages(rawData) {
+function scrapeSiteLinks(rawData) {
+
+  console.log(`================= finding: links =================`);
+
+  linkData = {
+    search: '<a href=',
+    fullLink: false
+  };
+
+  linkData.index = rawData.indexOf(linkData.search) + linkData.search.length;
+  linkData.indexStart = linkData.index;
+  while(rawData.indexOf(linkData.search) != -1) {
+  //for (var i = 0; i < 30; i++) {
+
+    while(!linkData.fullLink) {
+      linkData.index++;
+      linkData.fullLink = (rawData[linkData.index] != '"' && rawData[linkData.index] != ' ' && rawData[linkData.index] != "'") ? false : true;
+    }
+
+    linkData.fullLink = false;
+    linkData.link = rawData.slice(linkData.indexStart + 1, linkData.index);
+    rawData = rawData.replace(linkData.search + rawData[linkData.indexStart] + linkData.link + rawData[linkData.indexStart], '');
+    linkData.index = rawData.indexOf(linkData.search) + linkData.search.length;
+    linkData.indexStart = linkData.index;
+    scrape.siteLinks[scrape.siteLinks.length] = linkData.link
+  }
+
+  
+  console.log(scrape.siteLinks);
+
+} // end scrapeSiteLinks()
+
+function scrapeSiteImages(rawData) {
+
+  console.log(`================= finding: images =================`);
+
   for (x in scrape.imgType) {
     var imgData = {
       index: 0,
@@ -16,36 +53,35 @@ function scrapeImages(rawData) {
       fullURL: false,
       indexBack: 0,
       url: "",
-    }
-    console.log(`================= finding: ${scrape.imgType[x]} =================`);
+    };
+
     imgData.index = rawData.indexOf(`.${scrape.imgType[x]}`);
     imgData.indexBack = imgData.index;
+    
     while (imgData.index != -1) {
 
       while(!imgData.fullURL) {
         imgData.indexBack--;
         // find index of the first double quote, single quote, space, and left parenthesis
-        imgData.fullURL = (rawData[imgData.indexBack] != '"' && rawData[imgData.indexBack] != ' ' && rawData[imgData.indexBack] != '(') ? false : true;
+        imgData.fullURL = (rawData[imgData.indexBack] != '"' && rawData[imgData.indexBack] != ' ' && rawData[imgData.indexBack] != '(' && rawData[imgData.indexBack] != "'") ? false : true;
       } // end while
+
       imgData.fullURL = false;
-
-      //console.log(`Initial Index: ${imgData.index} Index Value: ${rawData[imgData.index]}`);
-      //console.log(`Last Index: ${imgData.indexBack} Last Value: ${rawData[imgData.indexBack]}`);
-
       imgData.url = rawData.slice(imgData.indexBack + 1, imgData.index + scrape.imgType[x].length + 1);
       rawData = rawData.replace(imgData.url, '');
+
       if (imgData.url.length > (scrape.imgType[x].length + 1)) {
-        console.log(`Real URL: ${imgData.url}`);
+        if (imgData.url.indexOf(`http`) || imgData.url[0] === '/') { // check if local url
+          imgData.url = scrape.url + imgData.url;
+        }
+        scrape.imgURL[x][scrape.imgURL[x].length] = imgData.url;
         imgData.totalImgs++;
-      } else {
-        console.log(`False Alarm: ${imgData.url}`);
       }
 
       imgData.index = rawData.indexOf(`.${scrape.imgType[x]}`);
       imgData.indexBack = imgData.index;
 
     } // end while
-
-    console.log();
   } // end for
-} // end scrapeImages()
+  console.log(scrape.imgURL);
+} // end scrapeSiteImages()
